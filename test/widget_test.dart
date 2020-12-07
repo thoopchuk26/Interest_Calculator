ff2859a07cbfb1d67c35d62febad514c391a00aa
thoopchuk <39135664+thoopchuk26@users.noreply.github.com>
2020-12-07 14:57:15 -0600

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:interest_calculator/calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:interest_calculator/main.dart';
+import 'package:interest_calculator/graph.dart';
 
 void main() {
-  
+  test('Test get Rate of Investment', (){
+    Calculator calc = new Calculator(10000.0,5.0,10.0,1000.0);
+    calc.investmentRate = calc.calculateInvestmentRate("Quarterly");
+    expect(calc.investmentRate,4.0);
+  });
+  test('Test Calculator Output', (){
+    Calculator calc = new Calculator(10000.0,5.0,10.0,1000.0);
+    calc.investmentRate = calc.calculateInvestmentRate("Annually");
+    double expectedVal = 28866.84;
+    double finalVal = calc.calculate(calc);
+    double finalRound = double.parse((finalVal).toStringAsFixed(2));
+    expect(finalRound,expectedVal);//Test that the final calculation is correctly equal to $28,866.84
+  });
+  test('Test Graph Output', (){
+    Calculator calc = new Calculator(10000.0,5.0,0.0, 1000.0);
+    calc.investmentRate = calc.calculateInvestmentRate("Annually");
+    var data = [(graph_display('0 Yr.', 10000, Colors.red))];
+    expect(Calculator.data.toString(),data.toString());//Test that values are accurately getting added to the graph builder
+  });
 }
